9abf8b419d6ec23d5576675b7c75f1fc94a22304
hassandlal <hassan.elsayed.hassan.26@gmail.com>
2021-09-28 17:15:57 +0200

    product details added and edited

--- lib/modules/item_details_Screen/item_details_Screen.dart
+++ lib/modules/item_details_Screen/item_details_Screen.dart
@@ -9,32 +9,34 @@ class ItemDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Container(
-              height: MediaQuery.of(context).size.height*0.4,
-              width: MediaQuery.of(context).size.width*0.9,
-              child: Image(image: AssetImage(model.imageUrl),fit: BoxFit.cover,),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Container(
+                height: MediaQuery.of(context).size.height*0.4,
+                width: MediaQuery.of(context).size.width*0.9,
+                child: Image(image: AssetImage(model.imageUrl),fit: BoxFit.cover,),
+              ),
             ),
-          ),
-          Center(child: Text(model.name,style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),)),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              'Description :',
-              style: TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.redAccent),
+            Center(child: Text(model.name,style: TextStyle(fontSize: 30,fontWeight: FontWeight.bold),)),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                'Production Description :',
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.redAccent),
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(model.description),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(model.description,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 15),),
+            )
+          ],
+        ),
       ),
     );
   }
